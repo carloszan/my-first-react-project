26fe05d13bb7d9a3c31f98b2515f753e473215b3
Carlos Zansavio <carloshlzansavio@gmail.com>
2021-09-18 00:26:13 -0300

    feat: change main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 const App = () => {
-  return <div>My first react app from kubernetes</div>;
+  return (
+    <>
+      <h1>Hi. My name is Carlos and this is the page.</h1>
+
+      <h2>We are working hard to make great products</h2>
+
+      <h2>I hope you come back</h2>
+
+      <h2>❤ Carlos</h2>
+    </>
+  );
 };
 
 export default App;
